3e2fcab1be61a1b215442d6ecf25c869a1ab6e78
Chris Forrest <redrider_cr@yahoo.com>
2021-03-16 22:11:26 -0400

    added ProjectsSection some styling and 3 projects

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import ProjectSection from './components/projectsSection';
+import styled from 'styled-components';
+
+
+
 
 function App() {
 
   const [ projectsSection, projectsSectionExpanded ] = useState(false);
   const [ resumeSection, resumeSectionExpanded ] = useState(false);
   const [ contactSection, contactSectionExpanded ] = useState(false);
+
+
+
 
 
   return (
@@ -16,8 +24,16 @@ function App() {
       </header>
       <body>
         <button onClick= {() => projectsSectionExpanded(true)}>PROJECTS</button>
+        {projectsSection && (
+          <div className='projectsSection'>
+          <ProjectSection  />
+
+          <button onClick={() => projectsSectionExpanded(false)} >Hide</button>
+         </div>
+        )}
         <button onClick= {() => resumeSectionExpanded(true)}>RESUME</button>
         <button onClick= {() => contactSectionExpanded(true)}>CONTACT</button>
+        
       </body>
     </div>
   );
