bc905d159e1d1a71c5c279bef7d01699d9a6553b
Chris Forrest <redrider_cr@yahoo.com>
2021-05-11 20:56:57 -0400

    added 2 images to the carousel

--- myportfolio/src/components/carousel.js
+++ myportfolio/src/components/carousel.js
@@ -6,6 +6,35 @@ export class BootstrapCarousel extends Component {
     render(){
         return (
             <div>
+                <div class ='container-fluid'>
+                    <div className="row title" style={{ marginBottom: "20px"}}>
+                        <div class="col-sm-12 btn-warning">
+                            About Section
+                        </div>
+                    </div>
+                </div>
+                <div className='container-fluid'>
+                    <Carousel>
+                        <Carousel.Item style={{'heigh':"300px"}}>
+                            <img style={{'height':'300px'}}
+                            className="d-block w-100"
+                            src={'assets/img/fallwalk.jpg'} 
+                            alt='fall foliage red and gold colored leaves'/>
+                            <Carousel.Caption>
+                                <h3>image one</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item style={{'height':'300px'}}>
+                            <img style={{'height':'300px'}}
+                            className="d-block w-100"
+                            src={'assets/img/mebjoshkayak.jpg'} 
+                            alt='three people kayaking in a river'/>
+                            <Carousel.Caption>
+                                <h3>image 2</h3>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
 
             </div>
         )
