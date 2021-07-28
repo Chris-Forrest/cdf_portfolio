2b54e0361b3d0a8f5b22b6528c5699df7a924081
Chris Forrest <redrider_cr@yahoo.com>
2021-07-28 16:38:29 -0400

    made header mobile responsive

--- myportfolio/src/components/carousel.js
+++ myportfolio/src/components/carousel.js
@@ -1,5 +1,7 @@
 import React,{ Component } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import '../images/fallwalkcompressed.png';
+import '../images/3kayakcompressed.png'
 
 export class BootstrapCarousel extends Component {
 
@@ -15,10 +17,10 @@ export class BootstrapCarousel extends Component {
                 </div>
                 <div className='container-fluid'>
                     <Carousel>
-                        <Carousel.Item style={{'heigh':"300px"}}>
+                        <Carousel.Item interval={500} style={{'heigh':"300px"}}>
                             <img style={{'height':'300px'}}
-                            className="d-block w-100"
-                            src={'assets/img/fallwalk.jpg'} 
+                            class="d-block w-100"
+                            src='../images/fallwalkcompressed.png'
                             alt='fall foliage red and gold colored leaves'/>
                             <Carousel.Caption>
                                 <h3>image one</h3>
@@ -26,14 +28,15 @@ export class BootstrapCarousel extends Component {
                         </Carousel.Item>
                         <Carousel.Item style={{'height':'300px'}}>
                             <img style={{'height':'300px'}}
-                            className="d-block w-100"
-                            src={'assets/img/mebjoshkayak.jpg'} 
+                            class="d-block w-100"
+                            src={require('../images/3kayakcompressed.png')} 
                             alt='three people kayaking in a river'/>
                             <Carousel.Caption>
                                 <h3>image 2</h3>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
+                    <img src={require('../images/fallwalkcompressed.png')} alt="fall foliage red and yellow" />
                 </div>
 
             </div>
@@ -41,4 +44,4 @@ export class BootstrapCarousel extends Component {
     }
 }
 
-export default BootstrapCarousel
+export default BootstrapCarousel;
--- myportfolio/src/components/styledComponents.js
+++ myportfolio/src/components/styledComponents.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+
 export const StyledComponent = styled.div`
   display: flex;
   justify-content: space-around;
@@ -8,20 +9,20 @@ export const StyledComponent = styled.div`
 `;
 
 export const StyledProjectCard = styled.div`
-display: flex;
-flex-direction: row;
-border: 1px solid rgb(210, 210, 210);
-border-radius: 5px;
-box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
-margin-bottom: 8px;
-padding-bottom: 12px;
-background-color: #ccffe5;
-width: 27%; 
-positions: relative;
-flex-wrap: wrap;
-position: relative;
-justify-content: space-between;
-`
+  display: flex;
+  flex-direction: row;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 5px;
+  box-shadow: 10px 8px 12px -2px rgb(128, 127, 197);
+  margin-bottom: 8px;
+  padding-bottom: 12px;
+  background-color: #ccffe5;
+  width: 27%; 
+  positions: relative;
+  flex-wrap: wrap;
+  position: relative;
+  justify-content: space-between;
+`;
  export const StyledVerticalBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -34,9 +35,29 @@ export const StyledLink = styled.td`
   color:  #0000EE;
   text-decoration: underline;
   cursor: pointer;
-`
+`;
 
 export const StyledCSection = styled.div`
-display: flex;
-justify-content: space-around;
-`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const StyledHeader = styled.div`
+  background-color: #3044f2;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  font-family: 'New Tegomin', serif;
+
+  @media (max-width: 1007px){
+    background-color: #c5f075;
+  }
+
+  @media (max-width: 640px){
+    justify-content: space-between;
+    background-color:orange;
+  }
+`;
